5f0be31c74fd321883b8bc212517d0a6e60f4452
ippo <s295502@studenti.polito.it>
2022-03-12 01:29:24 +0100

    completly change structure of program

--- lab01/es6.c
+++ lab01/es6.c
@@ -3,31 +3,26 @@
 #include <signal.h>
 #include <unistd.h>
 
-int i=0;
+int pre_prec_sig=0;
+int prec_sig=0;
 
-void handler1(int sig){
-    i++;
-    if(i==0)
+void handler(int sig){
+    if(prec_sig==sig && pre_prec_sig==sig){
+        exit(0);  
+    }
+    if(prec_sig!=sig && prec_sig!=0){
         printf("success\n");
-    if(i==2 || i==-2)
+    }
+    if(prec_sig==sig){
         printf("insuccess\n");
-    if(i==3 || i==-3)
-        abort();  
-}
-
-void handler2(int sig){
-    i--;
-    if(i==0)
-        printf("success\n");
-    if(i==2 || i==-2)
-        printf("insuccess\n");
-    if(i==3 || i==-3)
-        abort();   
+    }
+    pre_prec_sig=prec_sig;
+    prec_sig=sig;
 }
 
 int main(){
-    signal(SIGUSR1, handler1);
-    signal(SIGUSR2, handler2);
+    signal(SIGUSR1, handler);
+    signal(SIGUSR2, handler);
     printf("pid=%d\n", getpid());
     while(1){pause();}
 
