45553cfc34adfa429eaf9def32eb5ccfc55b6ee1
ippo <s295502@studenti.polito.it>
2022-03-17 01:05:56 +0100

    add es3.c and es4.c files

---
+++ lab02/es3.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+
+#define BUFSIZE 512
+#define PATH 300
+
+int bpb_write(int fd,char* buf,int size){
+    for(int i=0; i<size; i++){
+        if(buf[i]==32){
+            buf[i]=' ';
+        }
+        if(buf[i]==10){
+            buf[i]='\n';
+        }
+    }
+    write(fd, buf, size);
+    return 0;
+}
+
+void cp_function(char *p_dir1, char *p_dir2){
+    DIR *dp;
+    struct dirent *dirp;
+    struct stat sb;
+    int fd1, fd2;
+    char buf[BUFSIZE], head[PATH], file_s[PATH], file_d[PATH], dir_s[PATH], dir_d[PATH];
+
+    dp=opendir(p_dir1);
+
+    while( (dirp=readdir(dp)) !=NULL){
+        if( (strcmp(".", dirp->d_name)!=0) && (strcmp("..", dirp->d_name)!=0) ){
+
+            strcpy(file_s, p_dir1);
+            strcat(file_s, dirp->d_name);
+            strcpy(file_d, p_dir2);
+            strcat(file_d, dirp->d_name);
+
+            stat(file_s, &sb);
+
+            if(S_ISDIR(sb.st_mode)){
+
+                strcpy(dir_s, file_s);
+                strcat(dir_s, "/");
+                strcpy(dir_d, file_d);
+                strcat(dir_d, "/");
+
+                if(mkdir(dir_d, O_RDWR)!=0){
+                    fprintf(stderr, "Error in creating folder\n");
+                    abort();
+                }
+
+                chmod(dir_d, S_IRUSR | S_IXUSR | S_IWUSR);
+                cp_function(dir_s, dir_d);
+            }else{
+
+                sprintf(head, "%s %ld\n", dirp->d_name, sb.st_size);
+
+                if((fd1=open(file_s, O_RDONLY))==-1){
+                    fprintf(stderr, "Error in opening fd1\n");
+                    abort();
+                }
+                
+                if((fd2=open(file_d, O_WRONLY | O_CREAT))==-1){
+                    fprintf(stderr, "Error in opening fd2\n");
+                    abort();
+                }
+                
+                chmod(file_d, S_IRUSR | S_IXUSR | S_IWUSR);
+
+                write(fd2, head, PATH);
+                while(read(fd1, buf, BUFSIZE)>0){
+                    bpb_write(fd2, buf, BUFSIZE);
+                }
+                
+            }
+        }
+    }
+
+    closedir(dp);
+}
+
+int main(int argc, char **argv){
+    struct stat sb1, sb2;
+
+    if(argc!=3){
+        printf("Errore");
+        abort();
+    }
+
+    stat(argv[1], &sb1);
+    stat(argv[2], &sb2);
+
+    if(!S_ISDIR(sb1.st_mode) || !S_ISDIR(sb2.st_mode)){
+        printf("Errore");
+        abort();
+    }
+
+    cp_function(argv[1], argv[2]);
+
+    return 0;
+}
---
+++ lab02/es4.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+// #include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <pthread.h>
+
+void* paral_visit(void *par){
+    DIR *d;
+    struct dirent *dirp;
+    struct stat sb;
+    pthread_t t;
+    char *name_d=(char*) par, file[257];
+
+    d=opendir(name_d);
+
+    while( (dirp=readdir(d)) !=NULL){
+        strcpy(file, name_d);
+        strcat(file, dirp->d_name);
+        stat(file, &sb);
+
+        if(S_ISDIR(sb.st_mode)){
+            if( (strcmp(".", dirp->d_name)!=0) && (strcmp("..", dirp->d_name)!=0) ){
+                strcat(file, "/");
+                pthread_create(&t, NULL, paral_visit, (void*) file);
+                pthread_join(t, NULL);
+                // paral_visit((void*) file);
+            }
+        }else{
+            fprintf(stdout, "Thread=%ld file=%s\n", pthread_self(), file);
+        }
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char **argv){
+    struct stat *sb=(struct stat*) malloc((argc-1)*sizeof(struct stat));
+    pthread_t *thr=(pthread_t*) malloc((argc-1)*sizeof(pthread_t));
+    int i;
+
+    if(argc<1){
+        printf("Too few arguments\n");
+        abort();
+    }
+
+    for (i=0; i<(argc-1); i++){
+        stat(argv[i+1], &sb[i]);
+        if(!S_ISDIR(sb[i].st_mode)){
+            printf("Argument is not a dir\n");
+            abort();
+        }
+        pthread_create(&thr[i], NULL, paral_visit, (void*) argv[i+1]);
+    }
+
+    for (i=0; i<(argc-1); i++){
+        pthread_join(thr[i], NULL);
+    }
+    
+    free(thr);
+    free(sb);
+
+    return 0;
+}
